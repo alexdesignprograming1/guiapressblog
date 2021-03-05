d28a02fbd9570ef26dd6fcef8032528790598c2a
Alex Silva <alexsilva10san@gmail.com>
2021-03-04 23:08:23 -0300

    Criação da paginas Articles

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,8 +1,95 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../categories/Category");
+const Article = require("./Article");
+const slugify = require("slugify");
 
-router.get("/articles", (req, res) => {
-  res.send("Aqui artigos")
+router.get("/admin/articles", (req, res) => {
+  Article.findAll({
+    include: [{model: Category}]
+  }).then(articles => {
+    res.render("admin/articles/index", {articles: articles})
+  })
 })
+
+router.get("/admin/articles/new", (req, res) => {
+  Category.findAll().then(categories => {
+    res.render("admin/articles/new", {categories: categories})
+  })
+})
+
+router.post("/articles/save", (req, res) => { 
+  const title = req.body.title;
+  const body = req.body.body;
+  const category = req.body.category;
+
+  Article.create({
+    title: title,
+    slug: slugify(title),
+    body: body,
+    categoryId: category
+  }).then(() => {
+    res.redirect("/admin/articles"); 
+  }).catch((err) => {
+    console.log(err)
+  })
+})
+
+router.post("/articles/delete", (req, res) => {
+  const id = req.body.id;
+  if(id != undefined){
+    if(!isNaN(id)){
+      Article .destroy({
+        where: {
+          id: id
+        }
+      }).then(() => {
+        res.redirect("/admin/articles");
+      })
+    }else { //Não for um número
+      res.redirect("/admin/articles");
+    }
+  }else { // NULL
+    res.redirect("/admin/articles")
+  }
+}) 
+
+router.get("/admin/articles/edit/:id", (req, res) => {
+  const id = req.params.id;
+
+  if(isNaN(id)){
+    res.redirect("/admin/articles");
+  }
+
+  Article.findByPk(id).then(article => {
+    if(article != undefined) {
+
+      Category.findAll().then(categories => {
+        res.render("admin/articles/edit", {categories: categories, article: article})
+      })
+
+    }else {
+      res.redirect("/");
+    }
+  }).catch(err => {
+    res.redirect("/")
+  })
+})
+
+// router.post("/articles/update", (req, res) => {
+//   const id = req.body.id;
+//   const title = req.body.title;
+//   const body = req.body.body;
+
+//   Article.update({title: title, slug: slugify(title)}, {
+//     where: {
+//       id: id,
+//       title: title,
+//       body: body
+//     }
+//   }).then(() => {
+//     res.redirect("/admin/articles");
+//   }) 
+// })
 
 module.exports = router;
--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -29,4 +29,54 @@ router.get("/admin/categories", (req, res) => {
   })
 })
 
+router.post("/categories/delete", (req, res) => {
+  const id = req.body.id;
+  if(id != undefined){
+    if(!isNaN(id)){
+      Category.destroy({
+        where: {
+          id: id
+        }
+      }).then(() => {
+        res.redirect("/admin/categories");
+      })
+    }else { //Não for um número
+      res.redirect("/admin/categories");
+    }
+  }else { // NULL
+    res.redirect("/admin/categories")
+  }
+}) 
+
+router.get("/admin/categories/edit/:id", (req, res) => {
+  const id = req.params.id;
+
+  if(isNaN(id)){
+    res.redirect("/admin/categories");
+  }
+
+  Category.findByPk(id).then(category => {
+    if(category != undefined) {
+      res.render("admin/categories/edit", {category: category})
+    }else {
+      res.redirect("/admin/categories");
+    }
+  }).catch(err => {
+    res.redirect("/admin/categories")
+  })
+})
+
+router.post("/categories/update", (req, res) => {
+  const id = req.body.id;
+  const title = req.body.title;
+
+  Category.update({title: title, slug: slugify(title)}, {
+    where: {
+      id: id
+    }
+  }).then(() => {
+    res.redirect("/admin/categories");
+  })
+})
+
 module.exports = router;  
